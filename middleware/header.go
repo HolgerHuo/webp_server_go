c7bb364bedb9253d4637159ed7b2d107ea73a0d4
Holger Huo <50446405+HolgerHuo@users.noreply.github.com>
2024-02-04 00:24:29 +0800

    add: process time header

---
+++ middleware/header.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Headers() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		start := time.Now()
+
+		err := c.Next()
+
+		diff := time.Since(start)
+		c.Append("X-Processing-Time", fmt.Sprintf("%dms", diff.Milliseconds()))
+
+		return err
+	}
+}
